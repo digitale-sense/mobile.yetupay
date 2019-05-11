d81020c11b5355d6934ac680aaa63e661cc750fc
Cm-pingouin <carlosmaloba4@gmail.com>
2019-05-11 07:48:08 +0200

    Fix Balance

--- model/dao/transaction.dao.php
+++ model/dao/transaction.dao.php
@@ -5,6 +5,13 @@ class TransactionDAO{
     public function __construct(){
         $this->pdo = ConnectionManager::getConnection();
     }
+
+    public function get_all_transaction_by_id($user_id){
+        $req = $this->pdo->prepare("SELECT * FROM transaction WHERE user_id = :user_id");
+        $req->execute(array('user_id' => $user_id));
+        $res = $req->fetchAll();
+        return $res;
+    }
 }
 
 ?>
