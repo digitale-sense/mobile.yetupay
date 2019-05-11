40943f8e08f3f8f13b39edc434522d8aca54a5a0
Cm-pingouin <carlosmaloba4@gmail.com>
2019-05-11 07:49:38 +0200

    Portefeuille features

--- view/page/portefeuille.php
+++ view/page/portefeuille.php
@@ -1,3 +1,11 @@
+<?php 
+    session_start();
+    // $user_id =$_SESSION['user_id'];
+    // $pwd = $_SESSION['password'];
+    //  require_once('../../controller/user/sold.php?user_id='.$user_id.'&password='.$pwd);
+
+    require_once('../../controller/user/sold.php');
+?>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
@@ -30,6 +38,8 @@
             </div>
         </nav>
     </header>
+
+    <!-- Screen balance -->
     <div class="row" id="bala-card">
         <div class="col s12">
             <div class="card z-depth-3 radius abstract">
@@ -39,27 +49,42 @@
                     </div>
                     <div class="row">
                         <div class="col s6" style="border-right:1px solid silver;">
-                            <span class="card-title center bold-style">300.00 $</span>
+                            <span class="card-title center bold-style"><?php   
+                                if(empty($message['code']))
+                                    echo $message['USD'].' '.'$';
+                                else
+                                    echo '-4';
+                            ?></span>
                         </div>
                         <div class="col s6">
-                            <span class="card-title center bold-style">5000.00 Fc</span>
+                            <span class="card-title center bold-style"><?php
+                                if(empty($message['code']))
+                                    echo $message['CDF'].' '.'Fc';
+                            ?></span>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <!-- End screen balance -->
+
+    <!-- Controlle button -->
     <div class="row back-top">
         <div class="col s6 center">
-            <a href="recharge.php" class="waves-effect btn-action waves-light yetu-blue btn">Recharger</a>
+            <a href="tel:*1222%23" class="waves-effect btn-action waves-light yetu-blue btn">Recharger</a>
         </div>
         <div class="col s6 center">
             <a href="transferer.php" class="waves-effect btn-action waves-light yetu-blue btn">Tranferer</a>
         </div>
     </div>
+    <!-- End Controlle button -->
+
+    <!-- Screen off transaction -->
     <div class="row back-top">
         <div class="col s12">
             <div class="card cr radius">
+               <?php for ($i=0; $i < 5; $i++) { ?>
                 <div class="card-content row">
                     <div class="col s2">
                         <a class="btn-floating waves-effect waves-light btn-small z-depth-0 white">
@@ -77,44 +102,11 @@
                         <p class="truncate grey-text"># PP784575-1643-B46396</p>
                     </div>
                 </div>
-                <div class="divider"></div>
-                <div class="card-content row">
-                    <div class="col s2">
-                        <a class="btn-floating waves-effect waves-light btn-small z-depth-0 white">
-                            <i class="material-icons white-text violet">file_download</i>
-                        </a>
-                    </div>
-                    <div class="col s10">
-                        <p class="grey-text right">
-                            <small>
-                                20/06/18
-                            </small>
-                        </p>
-                        <h6 class="truncate no-margin bolder">Recharge</h6>
-                        <p>20.000 FC</p>
-                        <p class="truncate grey-text"># PP784575-1643-B46396</p>
-                    </div>
-                </div>
-                <div class="divider"></div>
-                <div class="card-content row">
-                    <div class="col s2">
-                        <a class="btn-floating waves-effect waves-light btn-small z-depth-0 white">
-                            <i class="material-icons white-text violet">file_download</i>
-                        </a>
-                    </div>
-                    <div class="col s10">
-                        <p class="grey-text right">
-                            <small>
-                                20/06/18
-                            </small>
-                        </p>
-                        <h6 class="truncate no-margin bolder">Recharge</h6>
-                        <p>20.000 FC</p>
-                        <p class="truncate grey-text"># PP784575-1643-B46396</p>
-                    </div>
-                </div>
+                <div class="divider"></div>                  
+               <?php }?>
             </div>
         </div>
     </div>
+    <!-- End Screen off transaction -->
 </body>
 </html>
