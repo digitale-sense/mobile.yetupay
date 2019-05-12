fa0f4d4ae9ded468583fd4a19fc89f5b474ba0d1
DonelMayala <mayaladonnel8@gmail.com>
2019-05-12 16:07:48 +0000

    Navbar logo integration

--- view/page/navbar.php
+++ view/page/navbar.php
@@ -1,12 +1,14 @@
-<header>
-    <nav>
-        <div class="nav-wrapper">
-            <a href="#" class="brand-logo">Logo</a>
-            <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li><a href="sass.html">Sass</a></li>
-                <li><a href="badges.html">Components</a></li>
-                <li><a href="collapsible.html">JavaScript</a></li>
-            </ul>
-        </div>
-    </nav>
-</header>
+<nav>
+    <div class="nav-wrapper">
+        <a href="#" class="brand-logo">
+            <object class="my-1-2" data="../asset/images/yetupay_blanc.svg" height="50" type="image/svg+xml">
+                <embed class="my-1-2" src="../asset/images/yetupay_blanc.svg" height="50" type="image/svg+xml" />
+            </object>
+        </a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="sass.html">Sass</a></li>
+            <li><a href="badges.html">Components</a></li>
+            <li><a href="collapsible.html">JavaScript</a></li>
+        </ul>
+    </div>
+</nav>
