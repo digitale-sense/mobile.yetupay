9af30591e0a489c3bc341b52270f8b091a9415c4
DonelMayala <mayaladonnel8@gmail.com>
2019-05-12 15:58:09 +0000

    Joined navbar to 'Portefeuille.php'

--- view/page/portefeuille.php
+++ view/page/portefeuille.php
@@ -1,12 +1,13 @@
-<?php 
-    session_start();
-    // $user_id =$_SESSION['user_id'];
-    // $pwd = $_SESSION['password'];
-    //  require_once('../../controller/user/sold.php?user_id='.$user_id.'&password='.$pwd);
+<?php
+session_start();
+// $user_id =$_SESSION['user_id'];
+// $pwd = $_SESSION['password'];
+//  require_once('../../controller/user/sold.php?user_id='.$user_id.'&password='.$pwd);
 
-    require_once('../../controller/user/sold.php');
+require_once('../../controller/user/sold.php');
 ?>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,8 +18,12 @@
     <link rel="stylesheet" href="../asset/css/style.css">
     <title>Mon portefeuille</title>
 </head>
+
 <body>
     <header class="yetu-orange">
+        <?php
+            include_once('navbar.php');
+        ?>
         <nav class="z-depth-0 yetu-orange">
             <div class="nav-wrapper">
                 <div class="row">
@@ -49,18 +54,18 @@
                     </div>
                     <div class="row">
                         <div class="col s6" style="border-right:1px solid silver;">
-                            <span class="card-title center bold-style"><?php   
-                                if(empty($message['code']))
-                                    echo $message['USD'].' '.'$';
-                                else
-                                    echo '-4';
-                            ?></span>
+                            <span class="card-title center bold-style"><?php
+                                                                        if (empty($message['code']))
+                                                                            echo $message['USD'] . ' ' . '$';
+                                                                        else
+                                                                            echo '-4';
+                                                                        ?></span>
                         </div>
                         <div class="col s6">
                             <span class="card-title center bold-style"><?php
-                                if(empty($message['code']))
-                                    echo $message['CDF'].' '.'Fc';
-                            ?></span>
+                                                                        if (empty($message['code']))
+                                                                            echo $message['CDF'] . ' ' . 'Fc';
+                                                                        ?></span>
                         </div>
                     </div>
                 </div>
@@ -84,31 +89,32 @@
     <div class="row back-top">
         <div class="col s12">
             <div class="card cr radius">
-               <?php for ($i=0; $i < 5; $i++) { ?>
-                <div class="card-content row">
-                    <div class="col s2">
-                        <a class="btn-floating waves-effect waves-light btn-small z-depth-0 white">
-                            <i class="material-icons white-text violet">file_download</i>
-                        </a>
+                <?php for ($i = 0; $i < 5; $i++) { ?>
+                    <div class="card-content row">
+                        <div class="col s2">
+                            <a class="btn-floating waves-effect waves-light btn-small z-depth-0 white">
+                                <i class="material-icons white-text violet">file_download</i>
+                            </a>
+                        </div>
+                        <div class="col s10">
+                            <p class="grey-text right">
+                                <small>
+                                    20/06/18
+                                </small>
+                            </p>
+                            <h6 class="truncate no-margin bolder">Recharge</h6>
+                            <p>20.000 FC</p>
+                            <p class="truncate grey-text"># PP784575-1643-B46396</p>
+                        </div>
                     </div>
-                    <div class="col s10">
-                        <p class="grey-text right">
-                            <small>
-                                20/06/18
-                            </small>
-                        </p>
-                        <h6 class="truncate no-margin bolder">Recharge</h6>
-                        <p>20.000 FC</p>
-                        <p class="truncate grey-text"># PP784575-1643-B46396</p>
-                    </div>
-                </div>
-                <?php if ($i!=5-1) { ?>
-                <div class="divider"></div>
-                <?php }?>                
-               <?php }?>
+                    <?php if ($i != 5 - 1) { ?>
+                        <div class="divider"></div>
+                    <?php } ?>
+                <?php } ?>
             </div>
         </div>
     </div>
     <!-- End Screen off transaction -->
 </body>
+
 </html>
