06092eb96f4af0e90baa9152b324d1bde5dc44d7
DonelMayala <mayaladonnel8@gmail.com>
2019-05-12 12:35:41 +0000

    JS pass codes deleted

--- view/page/login.php
+++ view/page/login.php
@@ -71,27 +71,7 @@
 </body>
 <script src="../asset/js/pass-view.js"></script>
 <script>
-    document.addEventListener('DOMContentLoaded', function() {
-        var elems = document.querySelectorAll('.tooltipped');
-        var instances = M.Tooltip.init(elems, {
-            position: 'top'
-        });
-        passicons = document.querySelectorAll('.pass');
-        passicons.forEach(passicon => {
-            passicon.addEventListener('click', function(e) {
-                passID = e.target.id.substring(0, e.target.id.length - 1);
-                if (e.target.innerText == 'visibility') {
-                    e.target.innerText = 'visibility_off';
-                    e.target.title = "Cacher le mot de passe";
-                    document.querySelector('#' + passID).type = 'text';
-                } else {
-                    e.target.innerText = 'visibility';
-                    e.target.title = "Afficher le mot de passe";
-                    document.querySelector('#' + passID).type = 'password';
-                }
-            });
-        });
-    });
+    
     <?php if (isset($_GET['dev_key'])) { ?> DEV_KEY = '<?= $_GET['dev_key'] ?>';
     <?php } ?>
 </script>
