6206b805487dcb5e69c1ea0eca50ea2f41f75992
DonelMayala <mayaladonnel8@gmail.com>
2019-05-12 12:38:02 +0000

    Delete pass JS

--- view/page/sigin.php
+++ view/page/sigin.php
@@ -67,27 +67,6 @@
     </section>
 </body>
 <script>
-    document.addEventListener('DOMContentLoaded', function() {
-        var elems = document.querySelectorAll('.tooltipped');
-        var instances = M.Tooltip.init(elems, {
-            position: 'top'
-        });
-        passicons = document.querySelectorAll('.pass');
-        passicons.forEach(passicon => {
-            passicon.addEventListener('click', function(e) {
-                passID = e.target.getAttribute('data-input');
-                if (e.target.innerText == 'visibility') {
-                    e.target.innerText = 'visibility_off';
-                    e.target.title = "Cacher le mot de passe";
-                    document.querySelector('#' + passID).type = 'text';
-                } else {
-                    e.target.innerText = 'visibility';
-                    e.target.title = "Afficher le mot de passe";
-                    document.querySelector('#' + passID).type = 'password';
-                }
-            });
-        });
-    });
     <?php if (isset($_GET['dev_key'])) { ?> DEV_KEY = '<?= $_GET['dev_key'] ?>';
     <?php } ?>
 </script>
