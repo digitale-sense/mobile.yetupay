6b80e74ac82aa62c1c709145599b19387d8b3761
DonelMayala <mayaladonnel8@gmail.com>
2019-05-14 05:37:12 +0000

    sold upgrade

--- controller/user/sold.php
+++ controller/user/sold.php
@@ -9,13 +9,14 @@ if(isset($_GET['user_id'])){
     $user_id = htmlspecialchars($_GET['user_id']);
     // $password = htmlspecialchars($_GET['password']);  
     $user = new User($user_id,null,null,null,null,null,null,null,null,null,null,null,null,null);
-    $user_dao = new UserDAO(); 
-    if($user_dao->check_password($user)){  
-        $user = $user_dao->get_user_by_id($user_id);
-        $message = array_merge($message,array( "CDF" => $user->getCDFSold(), "USD" => $user->getUSDSold()));
-    }
-    else
-        $message = array_merge($message, array('code' => -4));
+    $user_dao = new UserDAO();
+    $user = $user_dao->get_user_by_id($user_id);
+    $message = array_merge($message, array("CDF" => $user->getCDFSold(), "USD" => $user->getUSDSold()));
+    // if($user_dao->check_password($user)){  
+        
+    // }
+    // else
+    //     $message = array_merge($message, array('code' => -4));
 }
 
 ?>
