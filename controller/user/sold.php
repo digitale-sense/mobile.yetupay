05bb5f18d57633b9021bf7a1659530e40719688c
DonelMayala <mayaladonnel8@gmail.com>
2019-05-14 05:27:04 +0000

    Remove sold pass

--- controller/user/sold.php
+++ controller/user/sold.php
@@ -1,14 +1,14 @@
 <?php
 $message = array();
-if(isset($_GET['user_id'],$_GET['password'])){
+if(isset($_GET['user_id'])){
     require_once('../../db_config/connection_manager.class.php');
     require_once('../../db_config/db_params.class.php');
     require_once('../../model/structure/user.class.php');
     require_once('../../model/dao/user.dao.php'); 
 
     $user_id = htmlspecialchars($_GET['user_id']);
-    $password = htmlspecialchars($_GET['password']);  
-    $user = new User($user_id,null,null,null,$password,null,null,null,null,null,null,null,null,null);
+    // $password = htmlspecialchars($_GET['password']);  
+    $user = new User($user_id,null,null,null,null,null,null,null,null,null,null,null,null,null);
     $user_dao = new UserDAO(); 
     if($user_dao->check_password($user)){  
         $user = $user_dao->get_user_by_id($user_id);
