ba2925d58bb2022a732edd26700f896641e7a5e3
DonelMayala <mayaladonnel8@gmail.com>
2019-05-18 01:56:56 +0000

    error init

---
+++ view/page/error.php
@@ -0,0 +1,61 @@
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../asset/css/font.css">
+    <link rel="stylesheet" href="../asset/css/materialise.css">
+    <script src="../asset/js/materialize.js"></script>
+    <link rel="stylesheet" href="../asset/css/style.css">
+    <title>Mode développeur</title>
+</head>
+
+<body>
+    <header class="yetu-orange">
+        <nav class="yetu-orange z-depth-0">
+            <div class="nav-wrapper">
+                <a href="home.php" class="brand-logo">
+                    <object class="my-1-2" data="../asset/images/yetupay_blanc.svg" height="50" type="image/svg+xml">
+                        <embed class="my-1-2" src="../asset/images/yetupay_blanc.svg" height="50" type="image/svg+xml" />
+                    </object>
+                </a>
+            </div>
+        </nav>
+        <nav class="z-depth-0 yetu-orange">
+            <div class="nav-wrapper">
+                <div class="row mt-1">
+                    <div class="col s10">
+                        <h5 class="white-text">Erreur</h5>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <div class="row back-top">
+        <div class="col s12">
+            <div class="card z-depth-3 radius">
+                <div class="row">
+                    <div class="container">
+                        <div class="col s2 py-2">
+                            <i class="material-icons medium red-text">sentiment_very_dissatisfied</i>
+                        </div>
+                        <div class="col s10 center py-2 red-text">
+                            <h5>Impossible de traiter cette démande</h5>
+                        </div>
+                        <div class="col s12 center">
+                            <a class="waves-effect btn-action yetu-blue waves-light btn-small mb-2 btn-flat white-text">Retourner à l'acceuil</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </div>
+</body>
+<script src="../asset/js/sidenav-initer.js"></script>
+<script src="../asset/js/dropdown-initer.js"></script>
+
+</html>
